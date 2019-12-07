cab37ae56e16c6b353f889ef12ec4a93243323bb
e <1660378230@qq.com>
2019-12-08 01:06:12 +0800

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,6 @@ module.exports = {
   apps : [{
     name: 'api',
     script: 'app.js',
-
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
